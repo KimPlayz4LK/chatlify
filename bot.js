cee25b3a6cbc0e4dfbbf85ec59f9d376d895069b
KimPlayz4LK <leokim060109@gmail.com>
2020-11-07 21:38:35 -0400

    First upload

---
+++ bot.js
@@ -0,0 +1,18 @@
+const client=require(`socket.io-client`);
+const bot=client(`http://localhost`);
+bot.on('connect',()=>{
+bot.emit(`new-user`,`<span style='color:#88f;'>ChatlifyBot</span>`);
+});
+bot.emit(`chat-message`,{message:`Hello i am ChatlifyBot! Use the /help command to start using me!`});
+bot.on(`chat-message`,data=>{
+if(data.message.startsWith(`/help`)){bot.emit(`chat-message`,{message:`
+<br>Here's a command list:<br>
+<ul>
+<li>/retype - Retypes the text wrote after the command</li>
+<li>/say - Same as the <i>/retype</i> command.</li>
+</ul>
+`})}
+if(data.message.startsWith(`hello`)){bot.emit(`chat-message`,{message:`Hello there, ${data.user}!`})}
+if(data.message.startsWith(`/retype `)){bot.emit(`chat-message`,{message:data.message.substring(8,data.message.length)})}
+if(data.message.startsWith(`/say `)){bot.emit(`chat-message`,{message:data.message.substring(5,data.message.length)})}
+});
---
+++ script.js
@@ -0,0 +1,75 @@
+const chatInput=document.getElementById(`chat-input-content`);
+function newChatMessage(user,message) {
+var msg=document.createElement(`div`);
+msg.classList=`chat-message`;
+var msgUser=document.createElement(`p`);
+msgUser.innerHTML=user;
+msgUser.classList=`chat-message-user`;
+var msgContent=document.createElement(`p`);
+msgContent.innerHTML=message;
+msgContent.classList=`chat-message-content`;
+var msgTimestamp=document.createElement(`p`);
+msgTimestamp.innerHTML=`${new Date().getFullYear()}.${new Date().getMonth()}.${new Date().getDate()} ${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()}`;
+msgTimestamp.classList=`chat-message-timestamp`;
+msg.appendChild(msgUser);
+msg.appendChild(msgContent);
+msg.appendChild(msgTimestamp);
+document.getElementById(`current-chat`).appendChild(msg);
+document.getElementById(`current-chat`).scrollTop=document.getElementById(`current-chat`).scrollHeight;
+}
+newChatMessage(`<srvr>Server</srvr>`,`Connecting to the server`);
+const socket=io();
+const username=prompt(`Enter your username`,`Guest_${Math.round(Math.random()*10000)}`);
+
+function sound(src){
+this.sound=document.createElement("audio");
+this.sound.src=src;
+this.sound.setAttribute("preload","auto");
+this.sound.setAttribute("controls","none");
+this.sound.style.display="none";
+document.body.appendChild(this.sound);
+this.play=function(){this.sound.play();}
+this.stop=function(){this.sound.pause();}
+}
+chatInput.focus();
+chatInput.addEventListener(`keyup`,e=>{
+if(e.keyCode==13){document.getElementById(`chat-input-send`).click();}
+});
+document.getElementById(`chat-input-send`).onclick=function(e){
+if(chatInput.value!=``){
+e.preventDefault();
+if(chatInput.value.trim().startsWith(`@`)){
+socket.emit(`private-message`,{user:username,message:chatInput.value,timestamp:new Date().getTime()});
+newChatMessage(`<privatemsg>>> ${chatInput.value.trim().split(` `)[0].replace(`@`,``)}</privatemsg>`,chatInput.value.replace(chatInput.value.split(` `)[0],``).trim());
+}else{
+socket.emit(`chat-message`,{user:username,message:chatInput.value,timestamp:new Date().getTime()});
+newChatMessage(`<mymsg>${username} (${socket.id})</mymsg>`,chatInput.value);
+}
+chatInput.value=``;
+}
+};
+
+socket.on(`chat-message`,data=>{new sound(`./sound.mp3`).play();newChatMessage(data.user,data.message);});
+socket.on(`private-message`,data=>{
+console.log(`private-message`);
+if(data.toId==socket.id||data.originalTo.toLowerCase()==`@everyone`){
+new sound(`./ping.mp3`).play();
+newChatMessage(`<privatemsg>${data.user}</privatemsg>`,data.message);
+}
+});
+socket.on(`server-message`,data=>{new sound(`./sound.mp3`).play();newChatMessage(`<srvr>Server</srvr>`,data);});
+socket.on(`error-message`,data=>{new sound(`./sound.mp3`).play();newChatMessage(`<err>Server</err>`,data);});
+socket.on(`custom-message`,data=>{new sound(data.sound||`./sound.mp3`).play();newChatMessage(data.user||`<custom>Server</custom>`,data.message);});
+socket.on(`user-list`,data=>{refreshUserList(data);});
+socket.on(`disconnect`,()=>{new sound(`./disconnect.mp3`).play();newChatMessage(`<err>Server</err>`,`You are disconnected from the server`);});
+socket.on(`connect`,()=>{socket.emit(`new-user`,username);new sound(`./connect.mp3`).play();newChatMessage(`<success>Server</success>`,`You are (re)connected to the server`);socket.emit(`get-users`);});
+
+function refreshUserList(users) {
+document.getElementById(`user-list`).innerHTML=``;
+for(var user in users){
+var el=document.createElement(`div`);
+el.innerHTML=`<user>${users[user]}<user><br><id>(${user})</id>`;
+if(user==socket.id){el.classList=`self-card`;}
+document.getElementById(`user-list`).appendChild(el);
+}
+}
